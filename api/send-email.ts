a59cc37b81388120085b2bd2a2e3f2b85dbba184
malllzz <aziz.ramadhan1026@gmail.com>
2026-09-24 15:36:48 +0700

    change email to gmail

--- api/send-email.ts
+++ api/send-email.ts
@@ -8,15 +8,11 @@ export default async function handler(req: any, res: any) {
 
   const { to, subject, html } = req.body;
 
-// Konfigurasi khusus untuk Outlook Personal (mengatasi timeout)
+// Konfigurasi SMTP Gmail
   const transporter = nodemailer.createTransport({
-    host: 'smtp-mail.outlook.com',
-    port: 587,
-    secure: false, // Harus false untuk port 587
-    tls: {
-      ciphers: 'SSLv3', // Memaksa protokol yang bisa diterima Outlook
-      rejectUnauthorized: false // Mengabaikan error sertifikat internal server
-    },
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Harus true untuk port 465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -26,7 +22,7 @@ export default async function handler(req: any, res: any) {
   // Saat mengirim email, pastikan 'from' HARUS SAMA dengan EMAIL_USER
   try {
     const info = await transporter.sendMail({
-      from: `"Sistem BAST" <${process.env.EMAIL_USER}>`, // Jangan pakai email lain di sini
+      from: `"Sistem BAST" <${process.env.EMAIL_USER}>`, 
       to: to,
       subject: subject,
       html: html
